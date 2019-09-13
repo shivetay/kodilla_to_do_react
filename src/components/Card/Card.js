34271e75e0a423f71a31f77001a87ec1c9de21b6
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-13 12:30:42 +0200

    add Icons and Cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.scss';
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+  }
+  render() {
+    return(
+      <section className={styles.component}>
+        <h3>{this.props.title}</h3>
+      </section>
+    )
+  }
+}
+
+export default Card;
