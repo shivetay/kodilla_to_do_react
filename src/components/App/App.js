93673375946fb73cbe681fab90d1eb299848bea6
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-15 13:08:04 +0200

    add list add to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './App.scss';
+import {pageContents, listData, settings} from '../../data/dataStore';
 import List from '../List/List.js';
-import {pageContents, listData} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+
 
 class App extends React.Component {
+  state = {
+    lists: this.props.lists || [],
+  }
+  static protoTypes = {
+    title: PropTypes.node,
+    lists: PropTypes.array,
+  }
+  static defaultProps = {
+  title: listData.description,
+  }
+  addList(title){
+    this.setState(state => (
+      {
+        lists: [
+          ...state.listData,
+          {
+            key: state.lists.length ? state.lists[state.lists.length-1].key+1 : 0,
+            title
+          }
+        ]
+      }
+    ));
+  }
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List {...listData} />
+          {this.state.lists.map(({key, ...listProps}) => (
+            <List key={key} {...listProps} />
+          ))}
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addList(title)}/>
+        </div>
       </main>
     )
   }
