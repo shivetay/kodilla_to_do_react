04432c327e327a3af3b254970d4b89158819d519
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-26 13:58:57 +0200

    change in page transitions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,16 @@ import FAQ from '../FAQ/FAQ';
 import MainLayout from '../MainLayout/MainLayout';
 import styles from './App.scss';
 import {BrowserRouter, Route} from 'react-router-dom';
-import {AnimatedSwitch} from 'react-router-transition';
+import {spring, AnimatedSwitch} from 'react-router-transition';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
       <AnimatedSwitch
-        atEnter={{ opacity: 0 }}
-        atLeave={{ opacity: 0 }}
-        atActive={{ opacity: 1 }}
+        atEnter={{ opacity: 0, scale: (1.2) }}
+        atLeave={{ opacity: bounce(0), scale: bounce(0.8) }}
+        atActive={{ opacity: bounce(1), scale: bounce(1) }}
+        mapStyles={mapStyles}
         className={styles.switchWrapper}
       >
         <Route exact path="/" component={Home} />
@@ -23,5 +24,19 @@ const App = () => (
     </MainLayout>
   </BrowserRouter>
 );
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
 
 export default App;
