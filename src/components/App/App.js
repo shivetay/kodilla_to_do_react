920b603ddfa03a40b88f67bd5ffd0d47d22df3a8
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-26 13:46:09 +0200

    add page transitions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,23 @@ import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import FAQ from '../FAQ/FAQ';
 import MainLayout from '../MainLayout/MainLayout';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import styles from './App.scss';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
-        <Route exact path='/faq' component={FAQ} />
-      </Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/info" component={Info} />
+        <Route exact path="/faq" component={FAQ} />
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
