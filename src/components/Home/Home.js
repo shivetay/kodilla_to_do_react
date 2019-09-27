fb903ba3f145b7d157cc92de0b9f7ca7135b2d28
Łukasz Dawidowicz <lddev@outlook.com>
2019-09-27 09:34:13 +0200

    import lisLinks and fix drag and drop

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Home.scss';
 // import {pageContents, listData} from '../../data/dataStore';
-import List from '../List/ListContainer';
-import Search from '../Search/SearchContainer';
+import ListLink from '../ListLinks/ListLink';
 import {DragDropContext} from 'react-beautiful-dnd';
 
 class Home extends React.Component {
@@ -26,28 +25,26 @@ class Home extends React.Component {
           result.destination.droppableId != result.source.droppableId
         )
       ){
-        // console.log({
-        //   id: result.draggableId,
-        //   dest: {
-        //     index: result.destination.index,
-        //     columnId: result.destination.droppableId,
-        //   },
-        //   src: {
-        //     index: result.source.index,
-        //     columnId: result.source.droppableId,
-        //   },
-        // });
-        moveCard;
+        moveCard({
+          id: result.draggableId,
+          dest: {
+            index: result.destination.index,
+            columnId: result.destination.droppableId,
+          },
+          src: {
+            index: result.source.index,
+            columnId: result.source.droppableId,
+          },
+        });
       }
     };
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         <DragDropContext onDragEnd={moveCardHandler}>
           {lists.map(listData => (
-            <List key={listData.id} {...listData} />
+            <ListLink key={listData.id} {...listData} />
           ))}
         </DragDropContext>
       </main>
